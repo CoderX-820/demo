6a18d69ad5c2cfce34c5a8f62682359988073890
夏伟 <1114891937@qq.com>
2022-07-15 15:08:14 +0800

    2

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)$h0yjq28!phutryo((y%#&6v&0bclc6uj_k8o@7$x)&fi#wi%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
